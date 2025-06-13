c11e0292349fbe490198b4e9ec669c2947d0f942
umtcntp <57725483+umtcntp@users.noreply.github.com>
2025-06-13 17:10:22 +0300

    Delete src/data/fetchSkipData.js

--- src/data/fetchSkipData.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export const fetchSkipData = async () => {
-    const response = await axios.get('https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft');
-    return response.data;
-}
